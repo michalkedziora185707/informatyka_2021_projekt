0e5c82ef615423b4d186daa8ea30907ce5bb4f42
michalkedziora185707 <michalkedziora14@gmail.com>
2021-11-11 22:09:48 +0100

    menu1

--- informatyka_2021_projekt/informatyka_2021_projekt.cpp
+++ informatyka_2021_projekt/informatyka_2021_projekt.cpp
@@ -1,42 +1,178 @@
 ﻿#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <iostream>
 using namespace std;
 using namespace sf;
+#define MAX_LICZBA_POZIOMOW 3
+
+
+class Menu
+{
+
+private:
+	sf::Font font;
+	sf::Text menu[MAX_LICZBA_POZIOMOW];
+	int selectedItem = 0;
+
+
+public:
+	Menu(float width, float height);
+	~Menu() {};
+	void przesunG();
+	void przesunD();
+	int getSelectedItem() { return selectedItem; }
+	void draw(sf::RenderWindow &window);
+};
+
+
+Menu::Menu(float width, float height){
+
+	if (!font.loadFromFile("arial.ttf"))
+	{
+		return;
+	}
+	
+	menu[0].setFont(font);	
+	menu[0].setFillColor(sf::Color::Cyan);
+	menu[0].setString("Nowa gra");
+	menu[0].setPosition(sf::Vector2f(width / 3, height / (MAX_LICZBA_POZIOMOW + 1) * 1));
+	menu[1].setFont(font);
+	menu[1].setFillColor(sf::Color::White);
+	menu[1].setString("Ostatnie wyniki");
+	menu[1].setPosition(sf::Vector2f(width / 3, height / (MAX_LICZBA_POZIOMOW + 1) * 2));
+	menu[2].setFont(font);
+	menu[2].setFillColor(sf::Color::White);
+	menu[2].setString("Wyjscie");
+	menu[2].setPosition(sf::Vector2f(width / 3, height / (MAX_LICZBA_POZIOMOW + 1) * 3));
+}
+
+void Menu::draw(sf::RenderWindow &window)
+{
+	for (int i = 0; i < MAX_LICZBA_POZIOMOW; i++)
+	{
+		window.draw(menu[i]);
+	}
+}
+
+
+void Menu::przesunG()
+{
+	if (selectedItem >= 0 && selectedItem < MAX_LICZBA_POZIOMOW)
+	{
+		menu[selectedItem].setFillColor(sf::Color::White);
+		menu[selectedItem].setStyle(sf::Text::Regular);
+		selectedItem--;
+		if (selectedItem < 0)
+			selectedItem = MAX_LICZBA_POZIOMOW - 1;
+		menu[selectedItem].setFillColor(sf::Color::Cyan);
+		menu[selectedItem].setStyle(sf::Text::Bold);
+	}
+	
+	
+}
+
+void Menu::przesunD()
+{
+	if (selectedItem >= 0 && selectedItem < MAX_LICZBA_POZIOMOW)
+	{
+		menu[selectedItem].setFillColor(sf::Color::White);
+		menu[selectedItem].setStyle(sf::Text::Regular);
+		selectedItem++;
+		if (selectedItem >= MAX_LICZBA_POZIOMOW)
+			selectedItem = 0;
+		menu[selectedItem].setFillColor(sf::Color::Cyan);
+		menu[selectedItem].setStyle(sf::Text::Bold);
+	}
+	
+}
+
+
+void myDelay(int opoznienie)
+{
+	sf::Clock zegar;
+	sf::Time czas;
+	while (1)
+	{
+		czas = zegar.getElapsedTime();
+		if (czas.asMilliseconds() > opoznienie)
+		{
+			zegar.restart();
+			break;
+		}
+			
+	}	
+}
+
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	sf::CircleShape shape(20);
 	shape.setFillColor(sf::Color::Green);
-
-	RectangleShape proba(Vector2f(300,600));
-	proba.setPosition(0, 0);
-	proba.setFillColor(Color::Blue);
-	RectangleShape proba1(Vector2f(500, 200));
-	proba1.setPosition(300, 0);
-	proba1.setFillColor(Color::Blue);
-	RectangleShape proba2(Vector2f(200, 200));
-	proba2.setPosition(, 0);
-	proba2.setFillColor(Color::Blue);
-
+	int menu_selected_flag = 0;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML demo");
+	Menu menu(window.getSize().x, window.getSize().y);
 	
 	while (window.isOpen())
 	{
+	
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+		
 			if (event.type == sf::Event::Closed)
 				window.close();
-		}
-		window.clear();
-		window.draw(shape);
-		window.draw(proba);
-		window.draw(proba1);
-		//window.draw(proba3);
-		window.display();
 		
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Up)
+				{
+					myDelay(250);
+					menu.przesunG();
+				}
+					
+				if (event.key.code == sf::Keyboard::Down)
+				{
+					myDelay(250);
+					menu.przesunD();
+				}
+				if (menu_selected_flag == 0)
+				{
+					if (event.key.code == sf::Keyboard::Enter && menu.getSelectedItem() == 0)
+					{
+						cout << "Uruchamiam gre..." << std::endl;
+						menu_selected_flag = 1;
+					}
+						
+					if (event.key.code == sf::Keyboard::Enter && menu.getSelectedItem() == 1)
+					{
+						cout << "Najlepsze wyniki..." << std::endl;
+						menu_selected_flag = 1;
+					}
+						
+					if (event.key.code == sf::Keyboard::Enter && menu.getSelectedItem() == 2)
+					{
+						cout << "Koniec gry..." << std::endl;
+						menu_selected_flag = 1;
+					}
+						
+				}
+				
+					
+			}			
+		}
+		
+
+
+		
+		window.clear();
+	if(menu_selected_flag==0)
+			menu.draw(window);
+		window.display();
 	}
+
 	return 0;
 }
+
 
 
 
