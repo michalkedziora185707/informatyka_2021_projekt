a1703ede0f81ecb947cca91efd0258ad699bbf86
michalkedziora185707 <michalkedziora14@gmail.com>
2021-11-11 22:04:18 +0100

    menu

--- informatyka_2021_projekt/informatyka_2021_projekt.cpp
+++ informatyka_2021_projekt/informatyka_2021_projekt.cpp
@@ -1,9 +1,24 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+using namespace std;
+using namespace sf;
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	sf::CircleShape shape(20);
 	shape.setFillColor(sf::Color::Green);
+
+	RectangleShape proba(Vector2f(300,600));
+	proba.setPosition(0, 0);
+	proba.setFillColor(Color::Blue);
+	RectangleShape proba1(Vector2f(500, 200));
+	proba1.setPosition(300, 0);
+	proba1.setFillColor(Color::Blue);
+	RectangleShape proba2(Vector2f(200, 200));
+	proba2.setPosition(, 0);
+	proba2.setFillColor(Color::Blue);
+
+	
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -14,7 +29,11 @@ int main()
 		}
 		window.clear();
 		window.draw(shape);
+		window.draw(proba);
+		window.draw(proba1);
+		//window.draw(proba3);
 		window.display();
+		
 	}
 	return 0;
 }
